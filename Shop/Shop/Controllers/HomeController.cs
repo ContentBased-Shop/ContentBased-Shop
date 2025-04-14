5ebf757e45eb764095e3d7b86e094597e365b108
Yesimvu <phamnguyenvu287@gmail.com>
2025-04-14 23:52:32 +0700

    Create InnerPageControler/ (UI Contact In Process)
    Create InnerPageControler/ (UI Contact In Process)

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -15,34 +15,6 @@ namespace Shop.Controllers
         {
             return View();
         }
-        public ActionResult Login()
-        {
-            return View();
-        }
-        public ActionResult Register()
-        {
-            return View();
-        }
-        public ActionResult ForgotPassword()
-        {
-            return View();
-        }
-        public ActionResult PreCart()
-        {
-            return View();
-        }
-      
-        public ActionResult Checkout()
-        {
-            return View();
-        }
-        public ActionResult Checkout_ShoppingCart()
-        {
-            return View();
-        }
-        public ActionResult Contact()
-        {
-            return View();
-        }
+        
     }
 }
---
+++ Shop/Shop/Controllers/InnerPageController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class InnerPageController : Controller
+    {
+        //
+        // GET: /InnerPage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public ActionResult PreCart()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Checkout_ShoppingCart()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
