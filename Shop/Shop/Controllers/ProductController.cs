af4b937937f47e8b1ab8f32fc07208d3646a4916
Yesimvu <phamnguyenvu287@gmail.com>
2025-04-29 20:42:29 +0700

    30/4 mệt quá nghỉ thôi ii

--- Shop/Shop/Controllers/ProductController.cs
+++ Shop/Shop/Controllers/ProductController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Shop.Models;
 
 namespace Shop.Controllers
 {
     public class ProductController : Controller
     {
+        SHOPDataContext data = new SHOPDataContext("Data Source=MSI;Initial Catalog=CuaHang2;Persist Security Info=True;Use" +
+               "r ID=sa;Password=123;Encrypt=True;TrustServerCertificate=True");
         //
         // GET: /Product/
 
@@ -41,7 +44,11 @@ namespace Shop.Controllers
         }
         public ActionResult ProductWishList()
         {
-            return View();
+            var products = (from p in data.HangHoas
+                            orderby p.TenHangHoa
+                            select p).ToList(); // lấy hết, không lọc IsInStock
+
+            return View(products);
         }
     }
 }
