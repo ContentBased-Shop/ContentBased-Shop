69015d5caa64ba90e09ffcee41fe440ba1b15664
Yesimvu <phamnguyenvu287@gmail.com>
2025-05-02 23:28:58 +0700

    fix hiển thị product

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -15,11 +15,55 @@ namespace Shop.Controllers
         #region TRANG-CHU
         public ActionResult Index()
         {
-            var products = (from p in data.HangHoas
-                            orderby p.TenHangHoa
-                            select p).ToList(); // lấy hết, không lọc IsInStock
+            var hangHoaFull = (from hh in data.HangHoas
 
-            return View(products);
+                                   // Gộp với tất cả biến thể theo MaHangHoa
+                               join bt in data.BienTheHangHoas
+                               on hh.MaHangHoa equals bt.MaHangHoa into btGroup
+                               from bienThe in btGroup.DefaultIfEmpty()
+
+                                   // Gộp với đánh giá
+                               join dg in data.DanhGias
+                               on hh.MaHangHoa equals dg.MaHangHoa into dgGroup
+                               from danhGia in dgGroup.DefaultIfEmpty()
+
+                               group new { bienThe, danhGia } by new
+                               {
+                                   hh.MaHangHoa,
+                                   hh.TenHangHoa,
+                                   hh.MaDanhMuc,
+                                   hh.MaThuongHieu,
+                                   hh.HinhAnh,
+                                   hh.MoTa,
+                                   hh.NgayTao
+                               } into g
+
+                               select new HangHoaViewModel
+                               {
+                                   MaHangHoa = g.Key.MaHangHoa,
+                                   TenHangHoa = g.Key.TenHangHoa,
+                                   MaDanhMuc = g.Key.MaDanhMuc,
+                                   MaThuongHieu = g.Key.MaThuongHieu,
+                                   HinhAnh = g.Key.HinhAnh,
+                                   MoTa = g.Key.MoTa,
+                                   NgayTao = g.Key.NgayTao.Value,
+
+                                   GiaGoc = g.Where(x => x.bienThe != null)
+                                              .Select(x => x.bienThe.GiaGoc)
+                                              .FirstOrDefault() ?? 0,
+                                   GiaKhuyenMai = g.Where(x => x.bienThe != null)
+                                                .Select(x => x.bienThe.GiaKhuyenMai)
+                                                .FirstOrDefault() ?? 0,
+
+                                                                   SoLuongTonKho = g.Where(x => x.bienThe != null)
+                                                 .Select(x => x.bienThe.SoLuongTonKho)
+                                                 .FirstOrDefault() ?? 0,
+                                   SoLuongDanhGia = g.Count(x => x.danhGia != null),
+                                   DanhGiaTrungBinh = g.Any(x => x.danhGia != null)
+                                               ? g.Average(x => (float?)x.danhGia.SoSao) ?? 0
+                                               : 0
+                               }).ToList();
+            return View(hangHoaFull);
         }
         #endregion
     }
---
+++ Shop/Shop/Models/HangHoaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class HangHoaViewModel
+    {
+        public string MaHangHoa { get; set; }
+        public string TenHangHoa { get; set; }
+        public string MaDanhMuc { get; set; }
+        public string MaThuongHieu { get; set; }
+        public string HinhAnh { get; set; }
+        public string MoTa { get; set; }
+        public DateTime NgayTao { get; set; }
+
+        public double GiaGoc { get; set; }
+        public double GiaKhuyenMai { get; set; }
+        public int SoLuongTonKho { get; set; }
+
+        public int SoLuongDanhGia { get; set; }
+        public float DanhGiaTrungBinh { get; set; }
+    }
+
+
+}
