20a0a4c0d6009d3ac85f95fcb7254ee8050b67de
Yesimvu <phamnguyenvu287@gmail.com>
2025-05-15 21:30:13 +0700

    save

--- Shop/Shop/Controllers/ProductController.cs
+++ Shop/Shop/Controllers/ProductController.cs
@@ -271,9 +271,54 @@ namespace Shop.Controllers
 
         #endregion
         #region ProductLapTopPC
-        public ActionResult ProductLapTopPC()
+        public ActionResult ProductLapTopPC(string maDm)
         {
-            return View();
+            // Hiện thị danh sách đã thêm vào ds yêu thích
+            string maKhachHang = Session["UserID"] as string;
+            List<string> dsYeuThich = new List<string>();
+
+            if (!string.IsNullOrEmpty(maKhachHang))
+            {
+                dsYeuThich = data.YeuThiches
+                                 .Where(y => y.MaKhachHang == maKhachHang)
+                                 .Select(y => y.MaHangHoa)
+                                 .ToList();
+            }
+
+            ViewBag.DanhSachYeuThich = dsYeuThich;
+            var selectedBienThes = (from bt in data.BienTheHangHoas
+                                    join ha in data.HinhAnhHangHoas
+                                    on bt.MaBienThe equals ha.MaBienThe into hinhAnhGroup
+                                    select new BienTheHangHoaViewModel
+                                    {
+                                        MaBienThe = bt.MaBienThe,
+                                        MaHangHoa = bt.MaHangHoa,
+                                        MauSac = bt.MauSac,
+                                        DungLuong = bt.DungLuong,
+                                        CPU = bt.CPU,
+                                        RAM = bt.RAM,
+                                        KichThuocManHinh = bt.KichThuocManHinh,
+                                        LoaiBoNho = bt.LoaiBoNho,
+                                        GiaBan = bt.GiaBan ?? 0,
+                                        GiaKhuyenMai = bt.GiaKhuyenMai ?? 0,
+                                        SoLuongTonKho = bt.SoLuongTonKho ?? 0,
+                                        UrlAnh = hinhAnhGroup.Select(x => x.UrlAnh).ToList()
+                                    }).ToList();
+            var hangHoas = data.HangHoas
+                     .Where(h => h.MaDanhMuc == maDm)
+                     .ToList();
+            // Nếu đã có danh sách mã thương hiệu cần lấy
+            var thuongHieuIds = hangHoas.Select(h => h.MaThuongHieu).Distinct().ToList();
+
+            var thuongHieus = data.ThuongHieus
+                .ToList();
+            var productCategory = new ProductCategory
+            {
+                BienTheHangHoas = selectedBienThes,
+                HangHoas = hangHoas,
+                ThuongHieus = thuongHieus
+            };
+            return View(productCategory);
         }
         #endregion
         #region Gaming
@@ -283,15 +328,109 @@ namespace Shop.Controllers
         }
         #endregion
         #region AnotherProduct
-        public ActionResult AnotherProduct()
+        public ActionResult AnotherProduct(string maDm)
         {
-            return View();
+            // Hiện thị danh sách đã thêm vào ds yêu thích
+            string maKhachHang = Session["UserID"] as string;
+            List<string> dsYeuThich = new List<string>();
+
+            if (!string.IsNullOrEmpty(maKhachHang))
+            {
+                dsYeuThich = data.YeuThiches
+                                 .Where(y => y.MaKhachHang == maKhachHang)
+                                 .Select(y => y.MaHangHoa)
+                                 .ToList();
+            }
+
+            ViewBag.DanhSachYeuThich = dsYeuThich;
+            var selectedBienThes = (from bt in data.BienTheHangHoas
+                                    join ha in data.HinhAnhHangHoas
+                                    on bt.MaBienThe equals ha.MaBienThe into hinhAnhGroup
+                                    select new BienTheHangHoaViewModel
+                                    {
+                                        MaBienThe = bt.MaBienThe,
+                                        MaHangHoa = bt.MaHangHoa,
+                                        MauSac = bt.MauSac,
+                                        DungLuong = bt.DungLuong,
+                                        CPU = bt.CPU,
+                                        RAM = bt.RAM,
+                                        KichThuocManHinh = bt.KichThuocManHinh,
+                                        LoaiBoNho = bt.LoaiBoNho,
+                                        GiaBan = bt.GiaBan ?? 0,
+                                        GiaKhuyenMai = bt.GiaKhuyenMai ?? 0,
+                                        SoLuongTonKho = bt.SoLuongTonKho ?? 0,
+                                        UrlAnh = hinhAnhGroup.Select(x => x.UrlAnh).ToList()
+                                    }).ToList();
+            var hangHoas = data.HangHoas
+                     .Where(h => h.MaDanhMuc == maDm)
+                     .ToList();
+            // Nếu đã có danh sách mã thương hiệu cần lấy
+            var thuongHieuIds = hangHoas.Select(h => h.MaThuongHieu).Distinct().ToList();
+
+            var thuongHieus = data.ThuongHieus
+                .ToList();
+            var productCategory = new ProductCategory
+            {
+                BienTheHangHoas = selectedBienThes,
+                HangHoas = hangHoas,
+                ThuongHieus = thuongHieus
+            };
+            return View(productCategory);
         }
+
+
+       
         #endregion
         #region PhuKien
-        public ActionResult PhuKien()
+        public ActionResult PhuKien(string maDm)
         {
-            return View();
+
+            // Hiện thị danh sách đã thêm vào ds yêu thích
+            string maKhachHang = Session["UserID"] as string;
+            List<string> dsYeuThich = new List<string>();
+
+            if (!string.IsNullOrEmpty(maKhachHang))
+            {
+                dsYeuThich = data.YeuThiches
+                                 .Where(y => y.MaKhachHang == maKhachHang)
+                                 .Select(y => y.MaHangHoa)
+                                 .ToList();
+            }
+
+            ViewBag.DanhSachYeuThich = dsYeuThich;
+            var selectedBienThes = (from bt in data.BienTheHangHoas
+                                    join ha in data.HinhAnhHangHoas
+                                    on bt.MaBienThe equals ha.MaBienThe into hinhAnhGroup
+                                    select new BienTheHangHoaViewModel
+                                    {
+                                        MaBienThe = bt.MaBienThe,
+                                        MaHangHoa = bt.MaHangHoa,
+                                        MauSac = bt.MauSac,
+                                        DungLuong = bt.DungLuong,
+                                        CPU = bt.CPU,
+                                        RAM = bt.RAM,
+                                        KichThuocManHinh = bt.KichThuocManHinh,
+                                        LoaiBoNho = bt.LoaiBoNho,
+                                        GiaBan = bt.GiaBan ?? 0,
+                                        GiaKhuyenMai = bt.GiaKhuyenMai ?? 0,
+                                        SoLuongTonKho = bt.SoLuongTonKho ?? 0,
+                                        UrlAnh = hinhAnhGroup.Select(x => x.UrlAnh).ToList()
+                                    }).ToList();
+            var hangHoas = data.HangHoas
+                     .Where(h => h.MaDanhMuc == maDm)
+                     .ToList();
+            // Nếu đã có danh sách mã thương hiệu cần lấy
+            var thuongHieuIds = hangHoas.Select(h => h.MaThuongHieu).Distinct().ToList();
+
+            var thuongHieus = data.ThuongHieus
+                .ToList();
+            var productCategory = new ProductCategory
+            {
+                BienTheHangHoas = selectedBienThes,
+                HangHoas = hangHoas,
+                ThuongHieus = thuongHieus
+            };
+            return View(productCategory);
         }
         #endregion
         public ActionResult ProductWishList()
